168182f9e4af7d6ed352f52fd1abce52976ecdfd
smanaman <sanvatsman@gmail.com>
2024-05-29 23:13:18 -0700

    codec

--- .vscode/new2.c
+++ .vscode/new2.c
@@ -1,31 +1,39 @@
- #include<stdio.h>
+#include<stdio.h>
 #include<string.h>
-float pr(int total,int sub){
-    return(float)total/sub;
+float calculate_percentage(int total,int number_subject){
+    return(float)total/number_subject; // percentage of total marks
 }
 
 int main(){
-char name[100];
-char subject[4][10]={"matha","hindi","gk","since"};
-int sub=4;
-int mark[50];
-int total=0;
-float per;
+char name[100]; // user name
+char subject[4][10]={"Math","Hindi","GK","Science"};  // Subject list
+int number_subject=4; // number of subjects
+int mark[50]; // user input marks
+int total=0; // Total sum of marks
+float percentage; // percentage of marks
 
-printf("Enter your name=>");
-scanf("%[^\n]s",&name);
-printf("%s\n",name);
+printf("Enter your name=>"); // Ask user to Enter name
+
+scanf("%[^\n]s",&name); // input your name and assign to name variable
+
+printf("Your name is : %s\n",name); // User will see his name on screen.
+
+
 for (int i = 0; i<4; i++)
 {
 
-    printf("%s\n ",subject[i]);
-   scanf("%d",&mark[i]);
-   total += mark[i];
+    printf("Subject : %s\n ",subject[i]); // print subject list
+
+    scanf("%d",&mark[i]); // assign marks into mark variable
+
+    total += mark[i]; // add marks to total
 
 }
-per=pr(total,sub);
-printf("%.2f",per);
-if(per<=90 & 80<=per){
-    printf("your gread is A");
-}
+
+
+percentage = calculate_percentage(total,number_subject); // assign total and number of subject
+
+printf("Your percentage is :  %.2f",percentage); // show user his percentage
+
+
 }
---
+++ .vscode/new6.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int age;
+    int amount;
+    char name[50];
+   
+    
+printf("Enter your name=>"); // Ask user to Enter name
+
+scanf("%[^\n]s",&name); // input your name and assign to name variable
+ printf("Enter your age:=");
+ scanf("%d",&age);
+ if(age<=18){
+printf("wel come to our web site\n");
+printf("enter your amount:=");
+scanf("%d",&amount);
+if(amount<=100){
+char subject[4][10]={"khakara","maggie","chocolet","milk"};  // item list
+
+int price[50]; // user input price
+int total=0; // Total sum of item
+
+
+
+
+for (int i = 0; i<4; i++)
+{
+
+    printf("Subject : %s\n ",subject[i]); // print subject list
+
+    scanf("%d",&price[i]); // assign marks into mark variable
+
+    total += price[i]; // add marks to total
+}
+
+if(total<=100){
+printf("your item total is:=%d",total);
+}
+else{
+    printf("your amount is biggger than 100 sorry");
+}
+}else{
+    printf("your amount bigger than 100 so sorry you can't enter our web site");
+}
+ }else{
+printf("your age not 18 so good by");
+ }
+}
